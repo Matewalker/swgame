27044141e8c4a69ba59a188f6414e5c719fce479
Matewalker <148473036+Matewalker@users.noreply.github.com>
2024-01-05 15:32:44 +0100

    Add files via upload

---
+++ egyjatekosmod.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+
+    if ( ! isset($_SESSION["id"])) {
+        header("Location: bejelentkezes.html");
+        exit();
+    }
+
+    $id = $_SESSION["id"];
+    $gyoztes = $_POST["winner"];
+
+    function kapcsolodas($kapcsolati_szoveg, $felhasznalonev = '', $jelszo = '') {
+        $pdo = new PDO($kapcsolati_szoveg, $felhasznalonev, $jelszo);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+    
+    $kapcsolat = kapcsolodas('mysql:host=localhost;dbname=starwarsgame;', 'root', '');
+
+    if($gyoztes == 1){
+        $updateQuery = "UPDATE users SET `pontok` = `pontok` + 1 WHERE ID = ?";
+        $stmtUpdate = $kapcsolat->prepare($updateQuery);
+
+        if ($stmtUpdate->execute([$id])) {
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . $stmtUpdate->errorInfo()[2];
+        }
+    }
+?>
